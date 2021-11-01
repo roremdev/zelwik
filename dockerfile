78eea75f4551f6779212da6b4a148998445c28e3
roremDev <rorem.94.developer@gmail.com>
2021-11-01 13:49:03 -0600

    testing action

--- dockerfile
+++ dockerfile
@@ -5,11 +5,9 @@ COPY package*.json ./
 COPY ./src ./src
 
 FROM base as development
-EXPOSE 3000
 RUN npm install
 CMD ["npm","run", "dev"]
 
 FROM base as production
-EXPOSE 8080
 RUN npm ci && npm run build
 CMD ["npm","start"]
--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,4 @@
-if (process.env.NODE_ENV !== 'production')
-    require('dotenv').config({ path: './src/config/.env' });
-
-
 export default {
-    port: process.env.PORT || 8080,
-    mode: process.env.MODE || process.env.NODE_ENV,
+    port: process.env.PORT,
+    mode: process.env.NODE_ENV,
 };
